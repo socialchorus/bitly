4cbbc9629bea5d4807c87533eef923ab56e478b5
Osazeme Usen <osazeme.usen@andela.com>
2020-02-26 21:16:59 +0100

    included parallel request feature to bitly gem

--- lib/bitly/v4/client.rb
+++ lib/bitly/v4/client.rb
@@ -10,37 +10,58 @@ module Bitly
       end
 
       def shorten(long_url, opts = {})
-        query = { body: { long_url: long_url }.merge(opts).to_json }
-        request = post('/shorten', query)
+        query = ->(url) { { body: { long_url: url }.merge(opts).to_json } }
+
+        request = if long_url.is_a?(Array)
+                    request_array = long_url.map do |url|
+                      post('/shorten', query.call(url))
+                    end
+                    parallel_requests(request_array)
+                  else
+                    post('/shorten', query.call(long_url)).run
+                  end
+
         Bitly::V4::Url.new(self, request)
       end
 
       def clicks_summary(short_url, opts = {})
-        request = get("/bitlinks/#{short_url}/clicks/summary", opts)
+        link = ->(url) { "/bitlinks/#{url}/clicks/summary" }
+
+        request = if short_url.is_a?(Array)
+                    request_array = short_url.map do |url|
+                      get(link.call(url), opts)
+                    end
+                    parallel_requests(request_array)
+                  else
+                    get(link.call(short_url), opts).run
+                  end
+
         Bitly::V4::Url.new(self, request)
       end
 
       def clicks(short_url, opts = {})
-        request = get("/bitlinks/#{short_url}/clicks", opts)
+        link = ->(url) { "/bitlinks/#{url}/clicks" }
+
+        request = if short_url.is_a?(Array)
+                    request_array = short_url.map do |url|
+                      get(link.call(url), opts)
+                    end
+                    parallel_requests(request_array)
+                  else
+                    get(link.call(short_url), opts).run
+                  end
+
         Bitly::V4::Url.new(self, request)
       end
 
       private
 
       def post(path_uri, opts = {})
-        if path_uri.is_a?(Array)
-          path_uri.map { |elem| requests(:post, elem, opts) }
-        else
-          requests(:post, path_uri, opts).run
-        end
+        requests(:post, path_uri, opts)
       end
 
       def get(path_uri, opts = {})
-        if path_uri.is_a?(Array)
-          path_uri.map { |elem| requests(:get, elem, opts) }
-        else
-          requests(:get, path_uri, opts).run
-        end
+        requests(:get, path_uri, opts)
       end
 
       def default_headers
@@ -53,17 +74,18 @@ module Bitly
       def requests(method, path_uri = '', opts = {})
         Typhoeus::Request.new(
           "#{@authority_uri}#{path_uri}",
-          method: method,
+          method: method.to_sym,
           body: (opts[:body] || {}),
           params: (opts[:params] || {}),
           headers: (opts[:headers] || {}).merge(default_headers)
         )
       end
 
-      def parallel_request(request_array, max_concurrency = 15)
-        hydra = Typhoeus::Hydra.new(max_concurrency: max_concurrency)
+      def parallel_requests(request_array)
+        hydra = Typhoeus::Hydra.new(max_concurrency: 15)
         requests = request_array.map do |request|
           hydra.queue(request)
+          request
         end
         hydra.run
         requests
